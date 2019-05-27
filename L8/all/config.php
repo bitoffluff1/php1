29fc56bb40ffc380bb3c375c0c153298a7a2b5a3
bitoffluff1 <Lena_khmelnikova@mail.ru>
2019-05-27 13:15:04 +0300

    ДЗ 8
    Signed-off-by: bitoffluff1 <Lena_khmelnikova@mail.ru>

---
+++ L8/all/config.php
@@ -0,0 +1,29 @@
+<?php
+
+const ADMIN_KEY = "_ADMIN_";
+const LOGIN = "__YES__";
+const SALT = "ds1fe3atv435dafs435rf";
+
+function connect()
+{
+    static $link;
+    if (empty($link)) {
+        $link = mysqli_connect("localhost", "root", "", "gbphp");
+    }
+    return $link;
+}
+
+function clearStr($str)
+{
+    return mysqli_real_escape_string(connect(), strip_tags(trim($str)));
+}
+
+
+function notAdmin($message = "Only for admin")
+{
+    if (empty($_SESSION["adminKey"]) || $_SESSION["adminKey"] != ADMIN_KEY) {
+        $_SESSION["message"] = $message;
+        header("Location: ?pages=auth");
+        exit();
+    }
+}
---
+++ L8/all/pages/productsPage.php
@@ -0,0 +1,36 @@
+<?php
+function index()
+{
+    $sql = "SELECT id, address, name, price FROM gallery";
+    $res = mysqli_query(connect(), $sql);
+
+    while ($row = mysqli_fetch_assoc($res)) {
+        $item .= <<<php
+                <div class="item">
+                    <div class="fetured-item1">
+                        <a href="?id={$row['id']}&pages=singleProduct" class="fetured-item">
+                           <img src="{$row['address']}" alt="fetured-items">
+                           <div class="item-text">
+                               <p class="name-item">{$row['name']}</p>
+                               <p class="pink-item">\${$row['price']}</p>
+                           </div>
+                        </a>
+                    </div>
+                    <div class="add">
+                        <a href="?id={$row['id']}&func=addItem&pages=cart" class="add-to-card">
+                        <img class="cart-white" src="img/cart-white.svg" alt="cart">Add to Cart</a>
+                    </div>
+                </div>
+php;
+    }
+
+    $html = <<<php
+                <main>
+                    <div class="fetured-items-box container">$item</div>
+                </main>
+php;
+
+    return $html;
+}
+
+
